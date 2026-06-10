0e48e0288ffce6d6a4d666ca151f113dd8884dc7
alfredofperi-lgtm <alfredofperi@gmail.com>
2026-06-10 14:13:52 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,10 @@
-const CACHE = pendientes-v3
+const CACHE = 'pendientes-v4';
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
   './icons/icon-192.png',
-  './icons/icon-512.png',
-  'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=DM+Serif+Display&display=swap'
+  './icons/icon-512.png'
 ];
 
 self.addEventListener('install', e => {
@@ -24,6 +23,6 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html')))
+    caches.match(e.request).then(cached => cached || fetch(e.request))
   );
 });
